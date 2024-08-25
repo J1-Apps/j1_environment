220ec09615ee10f86ffbf49135e373cd159a24f5
Jaewon Yang <j.yang@draftkings.com>
2024-08-25 16:24:16 -0400

    #1: initial implementation

--- lib/j1_environment.dart
+++ lib/j1_environment.dart
@@ -1,7 +1,30 @@
 library j1_environment;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter/material.dart";
+import "package:get_it/get_it.dart";
+import "package:j1_crash_handler/j1_crash_handler.dart";
+import "package:j1_logger/j1_logger.dart";
+import "package:j1_router/j1_router.dart";
+
+final locator = GetIt.instance;
+
+abstract class J1Environment {
+  FirebaseOptions? get firebaseOptions;
+  J1CrashHandler get crashHandler;
+  J1Logger get logger;
+  J1Router get router;
+
+  Future<void> configure() async {
+    if (firebaseOptions != null) {
+      // coverage:ignore-start
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(options: firebaseOptions);
+      // coverage:ignore-end
+    }
+
+    crashHandler.configure();
+    locator.registerSingleton<J1Logger>(logger);
+    locator.registerSingleton<J1Router>(router);
+  }
 }
--- test/j1_environment_test.dart
+++ test/j1_environment_test.dart
@@ -1,12 +1,31 @@
-import 'package:flutter_test/flutter_test.dart';
+import "package:firebase_core/firebase_core.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:j1_crash_handler/j1_crash_handler.dart";
+import "package:j1_environment/j1_environment.dart";
+import "package:j1_logger/j1_logger.dart";
+import "package:j1_router/j1_router.dart";
 
-import 'package:j1_environment/j1_environment.dart';
+class _TestEnvironment extends J1Environment {
+  @override
+  FirebaseOptions? get firebaseOptions => null;
+
+  @override
+  J1CrashHandler get crashHandler => const LocalCrashHandler();
+
+  @override
+  J1Logger get logger => LocalLogger();
+
+  @override
+  J1Router get router => GoRouter();
+}
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group("J1 Environment", () {
+    test("configures common dependencies", () {
+      _TestEnvironment().configure();
+
+      expect(locator.get<J1Logger>() is LocalLogger, true);
+      expect(locator.get<J1Router>() is GoRouter, true);
+    });
   });
 }
